f65cdd807ed86c2cb9ef84e263fe21d59cbbfbae
Joao Victor Calvo Fracasso <joao.fracasso@outlook.com>
2020-08-09 10:50:11 -0300

    Add setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+  
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This repository contains a classification of Brazilian paper money.',
+    author='Joao Victor Calvo Fracasso',
+    license='BSD-3',
+)
