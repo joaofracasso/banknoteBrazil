a7a8dc408258dca16fa1769eb2096a81e3a153d3
Joao Victor Calvo Fracasso <joao.fracasso@outlook.com>
2020-08-09 11:16:32 -0300

    Removi pastas

---
+++ src/models/predict_model.py
@@ -0,0 +1,24 @@
+from torchvision import models
+import torchvision.transforms as transforms
+from PIL import Image
+
+model = models.densenet121(pretrained=True)
+model.eval()
+
+def transform_image(image_bytes):
+    my_transforms = transforms.Compose([transforms.Resize(255),
+                                        transforms.CenterCrop(224),
+                                        transforms.ToTensor(),
+                                        transforms.Normalize(
+                                            [0.485, 0.456, 0.406],
+                                            [0.229, 0.224, 0.225])])
+    image = Image.open(io.BytesIO(image_bytes))
+    return my_transforms(image).unsqueeze(0)
+
+
+def get_prediction(image_bytes):
+    tensor = transform_image(image_bytes=image_bytes)
+    outputs = model.forward(tensor)
+    _, y_hat = outputs.max(1)
+    predicted_idx = str(y_hat.item())
+    return predicted_idx
