1ad1e3833e7f0a400c6d2c84c30b57bebd7d7240
Joao Victor Calvo Fracasso <joaofracasso@users.noreply.github.com>
2021-03-02 18:06:39 -0300

    Dev (#9)
    * Correcao path
    
    * lint correction
    
    * correcao path
    
    * update action
    
    * add shell for validation
    
    * add new model

--- download_datasets.sh
+++
@@ -1,7 +0,0 @@
-echo "Downloading reais bankbnote dataset. This may take a while"
-wget -c -q --show-progress -O ./dataset.zip https://www.dropbox.com/s/jq4naabiu0a1z9r/data.zip?dl=0
-
-echo "Extracting Sid dataset..."
-unzip dataset.zip; rm dataset.zip
-
-echo "Finished."
