5d15b0a3217e922641f1b2cbec58307c49accc4b
Joao Victor Calvo Fracasso <joao.fracasso@outlook.com>
2021-03-02 08:20:39 -0300

    teste

--- app/src/modeling/predict_model.py
+++ app/src/modeling/predict_model.py
@@ -22,7 +22,7 @@ class_map = {
             }
 def transform_image(image_bytes):
     my_transforms = transforms.Compose([
-            transforms.Resize([224,224]),
+            transforms.Resize([224, 224]),
             transforms.ToTensor(),
             transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
         ])
