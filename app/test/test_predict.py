b0ebf600d77b0a9513056d27566d714d77317e64
Joao Victor Calvo Fracasso <joaofracasso@users.noreply.github.com>
2021-03-02 16:46:28 -0300

    Dev (#5)
    * teste
    
    * lint
    
    * add path
    
    * correcao action
    
    * bug fix
    
    * Merge branch 'master' into dev
    
    * Correcao path
    
    * lint correction

--- app/test/test_predict.py
+++ app/test/test_predict.py
@@ -1,7 +1,10 @@
-import sys
 import os
+import sys
+
 sys.path.insert(0, os.path.join(os.path.dirname(
     os.path.realpath(__file__)), "../"))
+import onnxruntime as ort
+
 import pytest
 from src.modeling.predict_model import get_prediction
 
@@ -9,7 +12,8 @@ files = "data/test/2reaisVerso/compressed_20_9551306.jpeg"
 
 @pytest.mark.parametrize('image', [files])
 def test_get_prediction(image):
+    ort_session = ort.InferenceSession('app/models/banknote_best.onnx')
     with open(image, 'rb') as f:
         image_bytes = f.read()
-        class_ = get_prediction(image_bytes)
+        class_ = get_prediction(image_bytes, ort_session)
     assert isinstance(class_, str)
