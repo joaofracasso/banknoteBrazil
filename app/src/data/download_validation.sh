f22064f4835a4d3b75cf3743787e4f73296162e3
Joao Victor Calvo Fracasso <joao.fracasso@outlook.com>
2021-12-05 20:31:29 +0000

    New template

--- app/src/data/download_validation.sh
+++
@@ -1,8 +0,0 @@
-#/bin/sh
-
-echo "Downloading reais bankbnote dataset. This may take a while"
-wget -c -q --show-progress -O ./validation.zip https://www.dropbox.com/s/est8xcd9blthj44/validation.zip?dl=0
-echo "Extracting validation dataset..."
-unzip validation.zip; rm validation.zip
-
-echo "Finished."
